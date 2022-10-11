67c01d695a3527cffe08f276b880db5a5c2d19e2
EnriqueDoster <enriquedoster@gmail.com>
2022-10-11 14:00:02 -0600

    update configs

--- run_AMR++.sh
+++ run_AMR++.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-#SBATCH -J AMR++ -o AMR++.out -t 01:00:00 -p short --ntasks-per-node=2
+#SBATCH -J AMR++ -o AMR++.out -t 01:00:00 -p short --ntasks-per-node=2 --mem 2G
+
+# This script works on TAMU's HPRC, but you need to follow the instructions on the Github to get the right conda 
+# environment installed on your computing environment
 
 nextflow run main_AMR++.nf -profile singularity_workshop --reads "/home/training/AMR_workshop_reads/small_subsample/*_{1,2}.fastq.gz" --pipeline standard_AMR
