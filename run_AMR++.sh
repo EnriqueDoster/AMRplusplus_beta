af2e6949864e7efbb2fe8bfd020f9495d9bbce5c
EnriqueDoster <enriquedoster@gmail.com>
2022-10-11 14:17:53 -0600

    update config

--- run_AMR++.sh
+++ run_AMR++.sh
@@ -3,5 +3,8 @@
 
 # This script works on TAMU's HPRC, but you need to follow the instructions on the Github to get the right conda 
 # environment installed on your computing environment
+module load Nextflow
+
+conda activate AMR++
 
 nextflow run main_AMR++.nf -profile conda_slurm --reads "/home/training/AMR_workshop_reads/small_subsample/*_{1,2}.fastq.gz" --pipeline standard_AMR
