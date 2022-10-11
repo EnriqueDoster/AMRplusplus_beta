0b9b5432fe0553b5e39232ac162a6cee4964071f
EnriqueDoster <enriquedoster@gmail.com>
2022-10-11 14:09:43 -0600

    update sbatch

--- run_AMR++.sh
+++ run_AMR++.sh
@@ -4,4 +4,4 @@
 # This script works on TAMU's HPRC, but you need to follow the instructions on the Github to get the right conda 
 # environment installed on your computing environment
 
-nextflow run main_AMR++.nf -profile singularity_workshop --reads "/home/training/AMR_workshop_reads/small_subsample/*_{1,2}.fastq.gz" --pipeline standard_AMR
+nextflow run main_AMR++.nf -profile conda_slurm --reads "/home/training/AMR_workshop_reads/small_subsample/*_{1,2}.fastq.gz" --pipeline standard_AMR
